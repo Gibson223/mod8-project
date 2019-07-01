ca186d22a8ad7f822ed98bbb2916d7c200be2788
Bjorn <>
2019-07-01 19:51:53 +0200

    Probably finished TypeChecker and added lots and lots of tests for it in TypeCheckerTest

--- src/sprockell/src/temp.hs
+++ src/sprockell/src/temp.hs
@@ -1,22 +1,20 @@
 import Sprockell
 
---	Bool a = true; while a {OutNumber 10;a = false;} OutNumber 5;
+--	Int b; Int c; if true {b = 10;} else {c = 20;}OutNumber b;
 prog :: [Instruction]
 prog = [
 {-0-}		Load (ImmValue 1) 2	{-  loaded true  -},
-{-1-}		Store 2 (DirAddr 0)	{-  declLine: varname: a stored at 0 types: [1] expr: true  -},
-{-2-}		Load (DirAddr 0) 2,
-{-3-}		Load (ImmValue 1) 3,
-{-4-}		Compute Xor 3 2 2,
-{-5-}		Branch 2 (Rel 6)	{- start while loop -},
-{-6-}		Load (ImmValue 10) 2	{-  loaded integer 10  -},
-{-7-}		WriteInstr 2 numberIO	{-  end OutNumber  -},
-{-8-}		Load (ImmValue 0) 2	{-  loaded false  -},
-{-9-}		Store 2 (DirAddr 0)	{-  asgnline: varname: a overwritten at heaploc 0 types: [1] expr: false  -},
-{-10-}		Jump (Rel (-8) )	{-  jump back to start of while loop  -},
-{-11-}		Nop	{-  after while  -},
-{-12-}		Load (ImmValue 5) 2	{-  loaded integer 5  -},
-{-13-}		WriteInstr 2 numberIO	{-  end OutNumber  -}
+{-1-}		Load (ImmValue 1) 3,
+{-2-}		Compute Xor 3 2 2,
+{-3-}		Branch 2 (Rel 4)	{- no if/elif conditional jump-},
+{-4-}		Load (ImmValue 10) 2	{-  loaded integer 10  -},
+{-5-}		Store 2 (DirAddr 0)	{-  asgnline: varname: b overwritten at heaploc 0 types: [0] expr: 10  -},
+{-6-}		Jump (Rel 3)			{- Jump to after if, elif and else statements -},
+{-7-}		Load (ImmValue 20) 2	{-  loaded integer 20  -},
+{-8-}		Store 2 (DirAddr 1)	{-  asgnline: varname: c overwritten at heaploc 1 types: [0] expr: 20  -},
+{-9-}		Nop,
+{-10-}		Load (DirAddr 0) 2,
+{-11-}		WriteInstr 2 numberIO	{-  end OutNumber  -}
 			,EndProg
 	]
 main = run [prog]
